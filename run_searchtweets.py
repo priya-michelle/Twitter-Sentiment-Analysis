0eef3dca8e82dde58a2f7901db5199591fba4fa9
krish-freenome <krish.sigler@freenome.com>
2020-07-08 16:47:53 -0700

    add partial code for searchtweets twitter api

---
+++ run_searchtweets.py
@@ -0,0 +1,18 @@
+# https://github.com/twitterdev/search-tweets-python
+# tweets are tweet_parser.tweet.Tweet
+
+from searchtweets import load_credentials, gen_rule_payload, collect_results
+
+
+def get_search_rule(query_term='', since='2019-07-24', count=100, lang='en', result_type='mixed'):
+    # pt_rule = f"{query_term} has:geo"
+    pt_rule=query_term
+    rule = gen_rule_payload(pt_rule, results_per_call=count)
+    return rule
+
+
+if __name__ == '__main__':
+    # expects a yaml file at ~/.twitter_keys.yaml
+    premium_search_args = load_credentials("~/.twitter_keys.yaml", yaml_key="search_tweets_premium")
+    rule = get_search_rule(query_term='biden')
+    tweets = collect_results(rule, result_stream_args=premium_search_args)
--- twitter_utils.py
+++ twitter_utils.py
@@ -14,10 +14,30 @@ def _get_api():
                   access_token_secret=access_token_secret)
     return api
 
+def _get_geocode_param():
+    """
+    We're using the coordinates of Tampa, with a radius of 334mi (distance tampa-pensacola).
+    This roughly corresponds to tweets from Florida.  More fine-grained filtering can be done downstream.
+    """
+    return "27.9506,-82.4572,334mi"
+
 def get_tweets(query='', since='2019-07-24', count=100, lang='en', result_type='mixed'):
-    # TODO add filters
-    # TODO maybe use **kwargs instead
+    """ Return tweets that match a given query
+
+    Given a query string, and optionally other search criteria, get a list of relevant tweets
+    Default to only returning tweets newer than July 24th 2019, in English.
+    Only return tweets that are geo-tagged.  The geocode param has the form latitude,longitude,radius.
+
+    See api docs for standard search: https://developer.twitter.com/en/docs/tweets/search/api-reference/get-search-tweets
+
+    General twitter search documentation: https://developer.twitter.com/en/docs/tweets/search/overview
+    
+    TODO maybe use **kwargs instead of specifying each individually
+    """
     api = _get_api()
+
+    geo_param = _get_geocode_param()
+
     query_string = f"q={query}&result_type={result_type}&since={since}&count={count}&lang={lang}"
     results = api.GetSearch(raw_query=query_string)
     return results
